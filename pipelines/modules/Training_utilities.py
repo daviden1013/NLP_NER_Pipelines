a891eaf79f7fc085a3d9b8e993ea431fe3c18629
David <daviden1013@gmail.com>
2023-02-05 14:08:35 -0600

    Add files via upload
    Included BIO_converter children for (MAE, BRAT)

--- pipelines/modules/Training_utilities.py
+++ pipelines/modules/Training_utilities.py
@@ -16,7 +16,7 @@ from tqdm import tqdm
 
 
 class BIO_converter:
-  def __init__(self, ann_dir:str, BIO_dir:str, mode:str):
+  def __init__(self, BIO_dir:str, mode:str):
     """
     This class inputs a directory with annotation files, outputs BIOs
 
@@ -29,7 +29,6 @@ class BIO_converter:
     mode : str
       choice of {'BIO', 'IO'} for output
     """
-    self.ann_dir = ann_dir
     self.BIO_dir = BIO_dir
     assert mode in {'BIO', 'IO'}, "mode must be one of {'BIO', 'IO'}"
     self.mode = mode
@@ -53,7 +52,7 @@ class BIO_converter:
     
   
   @abc.abstractmethod
-  def parse_annotation(self, ann_filename:str) -> Tuple[str, list]:
+  def parse_annotation(self) -> Tuple[str, list]:
     """
     This method inputs a annotation filename with dir
     outputs text content + list of tags 4-tuple 
@@ -137,6 +136,136 @@ class BIO_converter:
     This method iterate through all annotation files and output BIOs as csv format (.bio)
     """
     return NotImplemented
+
+
+class MAE_BIO_converter(BIO_converter):
+  def __init__(self, ann_dir:str, BIO_dir:str, mode:str):
+    self.ann_dir = ann_dir
+    super().__init__(BIO_dir, mode)
+  
+  def parse_annotation(self, ann_filename:str) -> Tuple[str, list]:
+    """
+    This method inputs a xml_filename with dir
+    outputs a text content. 
+    If has_tag == True, outputs text content + list of tags 4-tuple 
+    (tag_id, tag_name, start, end)
+    Parameters
+    ----------
+    xml_filename : str
+      xml filename with dir.
+    """
+    tree = ET.parse(ann_filename)
+    root = tree.getroot()
+    text = root.find('TEXT').text
+    
+    tags = []
+    for tag in root.find('TAGS'):
+      if tag.tag == 'HIGHLIGHT':
+        continue
+      tag_id = tag.attrib['id']
+      tag_name = tag.tag
+      start_pos, end_pos = tag.attrib['spans'].split('~')
+      if tag_name in ['CONSULT_DATETIME', 'LKW']:
+        att_type = tag.attrib['Type'] 
+      elif tag_name in ['tPA', 'TRANSPORTATION']:
+        att_type = tag.attrib['Modality'] 
+        
+      tags.append((tag_id, f"{tag_name}_{att_type}", int(start_pos), int(end_pos)))
+    
+    return text, tags
+
+  def pop_BIO(self):
+    ann_files = sorted([f for f in os.listdir(self.ann_dir) 
+                 if os.path.isfile(os.path.join(self.ann_dir, f)) and f[-4:] == '.xml'])
+    loop = tqdm(ann_files, total=len(ann_files), leave=True)
+    for ann_file in loop:
+      txt, ann = self.parse_annotation(ann_file)
+      
+      if self.mode == 'BIO':
+        bio_list = self._get_BIO(txt, ann)
+        filename = ann_file.replace('.xml', '.bio')
+      else:
+        bio_list = self._get_IO(txt, ann)
+        filename = ann_file.replace('.xml', '.io')
+        
+      with open(os.path.join(self.BIO_dir, filename), 'w', newline='', encoding='utf-8') as file:
+        csv_out=csv.writer(file)
+        csv_out.writerow(['token','start','end','label'])
+        for row in bio_list:
+          csv_out.writerow(row)
+
+
+class BRAT_BIO_converter(BIO_converter):
+  def __init__(self, txt_dir:str, ann_dir:str, BIO_dir:str, mode:str):
+    """
+    This class inputs a directory with annotation files, outputs BIOs
+    Parameters
+    ----------
+    txt_dir: str
+      Directory of text files
+    ann_dir : str
+      Directory of annotation files
+    BIO_dir : str
+      Directory of BIO files 
+    mode : str
+      choice of {'BIO', 'IO'} for output
+    """
+    self.txt_dir = txt_dir
+    self.ann_dir = ann_dir
+    self.BIO_dir = BIO_dir
+    assert mode in {'BIO', 'IO'}, "mode must be one of {'BIO', 'IO'}"
+    self.mode = mode
+  
+  def parse_annotation(self, txt_filename:str, ann_filename:str) -> Tuple[str, list]:
+    with open(os.path.join(self.txt_dir, txt_filename), 'r') as f:
+      text = f.read()
+        
+    ann_list = []
+    with open(os.path.join(self.ann_dir, ann_filename), 'r') as f:
+      lines = f.readlines()
+    for line in lines:
+      if line[0] == 'T':
+        l = line.split()
+        tag_id = l[0]
+        tag_name = l[1]
+        start = int(l[2])
+        i = 3
+        while True:
+          if ';' in l[i]:
+            i += 1
+          else:
+            end = int(l[i])
+            break
+            
+        ann_list.append((tag_id, tag_name, start, end))
+        
+    return text, ann_list
+  
+  
+  def pop_BIO(self):
+    """
+    This method iterate through annotation files and create BIO
+    """
+    txt_files = sorted([f for f in os.listdir(self.txt_dir) 
+                 if os.path.isfile(os.path.join(self.txt_dir, f)) and f[-4:] == '.txt'])
+    ann_files = sorted([f for f in os.listdir(self.ann_dir) 
+                 if os.path.isfile(os.path.join(self.ann_dir, f)) and f[-4:] == '.ann'])
+    loop = tqdm(zip(txt_files, ann_files), total=len(ann_files), leave=True)
+    for txt_file, ann_file in loop:
+      txt, ann = self.parse_annotation(txt_file, ann_file)
+      
+      if self.mode == 'BIO':
+        bio_list = self._get_BIO(txt, ann)
+        filename = ann_file.replace('.ann', '.bio')
+      else:
+        bio_list = self._get_IO(txt, ann)
+        filename = ann_file.replace('.ann', '.io')
+        
+      with open(os.path.join(self.BIO_dir, filename), 'w', newline='', encoding='utf-8') as file:
+        csv_out=csv.writer(file)
+        csv_out.writerow(['token','start','end','label'])
+        for row in bio_list:
+          csv_out.writerow(row)
 
 
 class BIO_feeder:
